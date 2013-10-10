1ee77c874011e943893a1b89d53002bcdc56e5dd
Shinmera <shinmera@tymoon.eu>
2013-10-10 11:01:53 +0200

    Updated class dependencies. Added docstrings. Finished get-posts.

--- conversation.lisp
+++ conversation.lisp
@@ -7,11 +7,12 @@
 (in-package :org.tymoonnext.ed-bot.xencl)
 
 (defclass conversation (meta-thread)
-  ((id :initarg :id :accessor id)
-   (op :initarg :op :accessor op)
-   (time :initarg :time :accessor post-time)
-   (title :initarg :title :accessor title))
+  ()
   (:documentation "Object for interacting with user conversations."))
+
+(defclass conversation-post (meta-post)
+  ()
+  (:documentation "A post in a private conversation thread."))
 
 (defmethod print-object ((conversation conversation) out)
   (print-unreadable-object (conversation out :type T)
@@ -23,11 +24,11 @@
       participants))
 
 (defmethod conversations ((user user))
+  "Retrieve the conversations the user has participated in. Note that this only works for the logged in user."
   (flet ((make-conversation (node)
-           (format T "~a" ($ node (serialize)))
            (make-instance 'conversation
                           :id (let ((id ($ node "h3.title a" (first) (attr :href) (node))))
-                                (subseq id (1+ (search "/" id)) (1- (search "/" id :from-end T))))
+                                (subseq id (1+ (search "/" id)) (search "/" id :from-end T)))
                           :op ($ node (attr :data-author) (node))
                           :time (let ((datetime ($ node ".posterDate .DateTime" (attr :data-time) (node))))
                                   (if (> (length datetime) 0)
@@ -38,6 +39,7 @@
        (each #'make-conversation :replace T))))
 
 (defmethod start-conversation ((user user) participants title message)
+  "Start a new conversation with the given participants."
   ($ (initialize (token-request "/conversations/insert"
                                 `(("recipients" . ,(to-participants participants))
                                   ("title" . ,title)
@@ -51,15 +53,21 @@
                  :title ($ ".titleBar h1" (text) (node))))
 
 (defmethod get-posts ((conversation conversation) &key)
-  ($ (initialize (token-request (concatenate 'string "/conversations/" (id conversation)) NIL) :type :HTML))
-  ;TODO: (crawl-posts)
-  )
+  "Retrieve all posts in the given conversation."
+  (let ((page (format NIL "/conversations/~a/" (id conversation))))
+    ($ (initialize (token-request page NIL) :type :HTML))
+    (when (search "Error" ($ "h1" (text) (node)))
+      (error 'forum-error :code 404 :page page :info ($ "label.OverlayCloser" (text) (node))))
+    (crawl-posts #'(lambda (node)
+                     (make-meta-post node conversation 'conversation-post)))))
 
 (defmethod post ((conversation conversation) message &key)
+  "Reply to a given conversation."
   (token-request (concatenate 'string "/conversations/" (id conversation) "/insert-reply")
                  `(("message_html" . ,message))))
 
 (defmethod invite ((conversation conversation) participants)
+  "Invite a new user to the given conversation."
   (assert (string-equal (id *user*) (op conversation)) ()
           'forum-error :code 4 :text (format NIL "Cannot invite users as you are not the OP (~a != ~a)." (id *user*) (op conversation)))
   (token-request (concatenate 'string "/conversations/" (id conversation) "/invite-insert")
