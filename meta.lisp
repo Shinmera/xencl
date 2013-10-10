2b89f439d17b6079d7241dbede063c3745bdebc5
Shinmera <shinmera@tymoon.eu>
2013-10-10 11:00:46 +0200

    Extended and added meta classes.

--- meta.lisp
+++ meta.lisp
@@ -6,18 +6,55 @@
 
 (in-package :org.tymoonnext.ed-bot.xencl)
 
-(defclass meta-thread ()
-  ()
-  (:documentation "Super-class for thread-like classes."))
+(defclass meta-forum ()
+  ((id :initarg :id :accessor id)
+   (title :initarg :title :accessor title))
+  (:documentation "Super-class for forum-like classes that contain threads."))
 
-(defgeneric get-posts (object &key &allow-other-keys))
-(defgeneric post (object message &key &allow-other-keys))
+(defmethod print-object ((forum meta-forum) out)
+  (print-unreadable-object (forum out :type T)
+    (format out "~a (~a)" (title forum) (id forum))))
+
+(defgeneric get-threads (object &key &allow-other-keys)
+  (:documentation "Retrieve all threads in a meta-forum."))
+(defgeneric start-thread (object message &key &allow-other-keys)
+  (:documentation "Create a new thread in a meta-forum."))
+
+(defclass meta-thread ()
+  ((id :initarg :id :accessor id)
+   (op :initarg :op :accessor op)
+   (time :initarg :time :accessor post-time))
+  (:documentation "Super-class for thread-like classes that contain posts."))
+
+(defmethod print-object ((thread meta-thread) out)
+  (print-unreadable-object (thread out :type T)
+    (format out "~a (~a)" (id thread) (op thread))))
+
+(defgeneric get-posts (object &key &allow-other-keys)
+  (:documentation "Retrieve all posts in a thread or post-flow"))
+(defgeneric post (object message &key &allow-other-keys)
+  (:documentation "Post a new message to a thread or post-flow."))
 
 (defclass meta-post ()
   ((id :initarg :id :accessor id)
+   (thread :initarg :thread :accessor thread)
    (author :initarg :author :reader author)
    (message :initarg :message :accessor message)
    (time :initarg :time :accessor post-time))
   (:documentation "Super-class for post-like classes."))
 
-(defgeneric reply (object message &key &allow-other-keys))
+(defmethod print-object ((post meta-post) out)
+  (print-unreadable-object (post out :type T)
+    (format out "~a/~a (~a)" (thread post) (id post) (author post))))
+
+(defgeneric reply (object message &key &allow-other-keys)
+  (:documentation "Reply to a thread or post-flow."))
+
+
+(defun make-meta-post (node &optional thread (class 'meta-post))
+  (make-instance class
+                 :id ($ node (attr :id) (node))
+                 :thread thread
+                 :author ($ node (attr :data-author) (node))
+                 :message ($ node "blockquote.messageText" (html) (node))
+                 :time (parse-post-datetime ($ node ".DateTime" (attr :title) (node)))))
