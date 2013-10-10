799b427e6c3c77fce7113d73ff7972c6c089b3a1
Shinmera <shinmera@tymoon.eu>
2013-10-10 13:10:42 +0200

    Finished thread loading.

--- forum.lisp
+++ forum.lisp
@@ -19,5 +19,18 @@
                                   (subseq href (1+ (search "/" href)) (search "/" href :from-end T)))
                             :title ($ node (text) (node)))))
 
-(defmethod get-threads ((forum forum) &key)
-  ())
+(defmethod get-threads ((forum forum) &key (start 0) (num 20))
+  "Retrieve all or a subset of threads in a forum."
+  (flet ((make-thread (node)
+           (make-instance 'forum-thread
+                          :id (let ((href ($ node ".titleText>.title>a" (node) (attr :href) (node))))
+                                (subseq href (1+ (search "/" href)) (search "/" href :from-end T)))
+                          :title ($ node ".titleText>.title>a" (text) (node))
+                          :op ($ node ".titleText .username" (text) (node))
+                          :time (parse-post-datetime ($ node ".titleText .DateTime" (text) (node))))))
+    (let ((page (format NIL "/forums/~a/" (id forum))))
+      ($ (initialize (token-request page NIL) :type :HTML))
+      (when (search "Error" ($ "h1" (text) (node)))
+        (error 'forum-error :code 404 :page page :info ($ "label.OverlayCloser" (text) (node))))
+      (crawl-nodes ".discussionListItems>li" #'make-thread
+                   :start start :num num))))
