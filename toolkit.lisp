84ee6ea10e922b005762c8bac9ee6891fd338c9c
Shinmera <shinmera@tymoon.eu>
2013-10-10 11:01:33 +0200

    Added page to error object. Added function to crawl a page-inated page. Added function to parse literal datestrings.

--- toolkit.lisp
+++ toolkit.lisp
@@ -13,11 +13,12 @@
 
 (define-condition forum-error (error)
   ((info :initarg :info)
-   (code :initarg :code))
+   (code :initarg :code)
+   (page :initarg :page))
   (:documentation "Condition signalled when a forum request returns an error."))
 
 (defmethod print-object ((obj forum-error) stream)
-  (format stream "~a (~a)" (slot-value obj 'info) (slot-value obj 'code)))
+  (format stream "~a (~a) ~a" (slot-value obj 'info) (slot-value obj 'code) (slot-value obj 'page)))
 
 (defun url (url) (concatenate 'string *index* url))
 
@@ -53,3 +54,48 @@
 (defun initiate (index user pass)
   (setf *index* index)
   (login (make-instance 'user :id user :pass pass)))
+
+(defun crawl-posts (&optional (post-parser #'make-meta-post))
+  (flet ((parse-posts ()
+           (loop for node in ($ ".messageList li")
+              collect (funcall post-parser node))))
+    (let ((posts (parse-posts))
+          (nav ($ ".PageNav" (node))))
+      (when nav
+        (loop with root = ($ nav "a[rel=\"start\"]" (attr :href) (node))
+           for i from 2 upto (parse-integer ($ nav (attr :data-last) (node)))
+           do ($ (initialize (token-request (format NIL "/~apage-~a" root i) NIL) :type :HTML))
+             (setf posts (append posts (parse-posts)))))
+      posts)))
+
+(defvar *monthnames-to-int*
+  '(:JAN 1 :FEB 2 :MAR 3 :APR 4 :MAY 5 :JUN 6 :JUL 7 :AUG 8 :SEP 9 :OCT 10 :NOV 11 :DEC 12))
+
+(defun parse-post-datetime (datetime)
+  (if (= 0 (length datetime))
+      NIL
+      (let* ((pos (search "at" datetime))
+             (date (subseq datetime 0 (1- pos)))
+             (time (subseq datetime (+ pos 3)))
+             
+             (pos (search " " time))
+             (ampm (subseq time (1+ pos)))
+             (time (subseq time 0 pos))
+             (pos (search ":" time))
+             (hours (parse-integer (subseq time 0 pos)))
+             (mins (parse-integer (subseq time (1+ pos))))
+             (hours (if (string= ampm "PM") (+ hours 12) hours))
+             
+             (pos (search "," date))
+             (year (parse-integer (subseq date (+ 2 pos))))
+             (date (subseq date 0 pos))
+             (pos (search " " date))
+             (day (parse-integer (subseq date (1+ pos))))
+             (month (string-upcase (subseq date 0 pos)))
+             (month (getf *monthnames-to-int* (find-symbol month "KEYWORD")))
+
+             (timestamp (local-time:make-timestamp)))
+
+        (local-time:adjust-timestamp! timestamp
+          (set :sec 0) (set :minute mins) (set :hour hours)
+          (set :year year) (set :month month) (set :day-of-month day)))))
