0e47037db73d1fdb2946a9d3768067d8ec159ec8
Shinmera <shinmera@tymoon.eu>
2013-10-10 11:02:01 +0200

    Added thread stump class.

---
+++ thread.lisp
@@ -0,0 +1,12 @@
+#|
+  This file is a part of XenCL
+  (c) 2013 TymoonNET/NexT http://tymoon.eu (shinmera@tymoon.eu)
+  Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package :org.tymoonnext.ed-bot.xencl)
+
+(defclass forum-thread (meta-thread)
+  ()
+  (:documentation "A thread in a forum category."))
+
